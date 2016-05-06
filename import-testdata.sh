cb233c298528639e6cb11e6296d42635c0c20de8
guersoy <halil-cem.guersoy@adesso.de>
2016-05-06 22:07:15 +0200

    allign testdata import with new container names

--- import-testdata.sh
+++ import-testdata.sh
@@ -7,9 +7,9 @@ echo "Setting up docker environment to connect to swarm..."
 # set docker host coordinates correctly
 eval $($DOCKER_MACHINE env --swarm swarm-1)
 
-echo "Retrieving information about elasticsearch node es-1..."
-ES1_NODE=$(docker inspect --format='{{.Node.IP}}' es-1)
-ES1_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9200/tcp") 0).HostPort}}' es-1)
+echo "Retrieving information about elasticsearch node elastic_esnode_1..."
+ES1_NODE=$(docker inspect --format='{{.Node.IP}}' elastic_esnode_1)
+ES1_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9200/tcp") 0).HostPort}}' elastic_esnode_1)
 
 echo "Downloading test data to node swarm-1..."
 $DOCKER_MACHINE ssh swarm-1 "wget https://www.elastic.co/guide/en/kibana/3.0/snippets/shakespeare.json"
